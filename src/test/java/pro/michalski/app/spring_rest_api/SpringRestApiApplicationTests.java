cc3fd88e61638a77e8bedb05e42eb1a5af43b58f
Marcin Michalski <mkamichalski@gmail.com>
2024-09-07 22:59:39 +0200

    first commit

---
+++ src/test/java/pro/michalski/app/spring_rest_api/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.michalski.app.spring_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
