a0fda9714e8245192eb6927c90dfb7baad8de4f6
Marcin Michalski <mkamichalski@gmail.com>
2024-09-08 12:29:45 +0200

    0.0.1.2409081228

--- src/main/java/pro/michalski/app/spring_rest_api/rest/Controller.java
+++ src/main/java/pro/michalski/app/spring_rest_api/rest/Controller.java
@@ -1,5 +1,10 @@
 package pro.michalski.app.spring_rest_api.rest;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users") //http://localhost:8080/users
 public class Controller {
 
+    @GetMapping(path = "/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "Get user with ID: " + userId;
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "Create user";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "Update user";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "Delete user";
+    }
 }
